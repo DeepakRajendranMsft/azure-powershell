5a9db949b40e00d7edffc0b3b6a3ed1a89c859ae
deepakswifty <rajendran.deepak@outlook.com>
2016-01-28 00:47:02 -0800

    prototype 1

---
+++ src/CLU/Microsoft.Azure.Commands.Network/VirtualNetwork1/SetAzureVirtualNetworkCommand.cs
@@ -0,0 +1,115 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Management.Automation;
+using AutoMapper;
+using Microsoft.Azure.Commands.Tags.Model;
+using Microsoft.Azure.Management.Network;
+using Microsoft.Azure.Commands.Network.Models;
+
+using MNM = Microsoft.Azure.Management.Network.Models;
+
+namespace Microsoft.Azure.Commands.Network
+{
+    using System.Collections.Generic;
+
+    [Cmdlet(VerbsCommon.Set, "AzureRmVirtualNetwork1")]
+    [OutputType(typeof(PSVirtualNetwork))]
+    [CliCommandAlias("vnet1 set")]
+    public class SetAzureVirtualNetwork1Command : VirtualNetworkBaseCmdlet
+    {
+        [Parameter(Mandatory = true, ValueFromPipeline = true, HelpMessage = "The virtualNetwork",
+            ParameterSetName = "object")]
+        public PSVirtualNetwork VirtualNetwork { get; set; }
+
+        [Alias("ResourceName")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, HelpMessage = "The resource name.",
+            ParameterSetName = "clu")]
+        [ValidateNotNullOrEmpty]
+        public virtual string Name { get; set; }
+
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, HelpMessage = "The resource group name.",
+            ParameterSetName = "clu")]
+        [ValidateNotNullOrEmpty]
+        public virtual string ResourceGroupName { get; set; }
+
+        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The address prefixes of the virtual network", ParameterSetName = "clu")]
+        [ValidateNotNullOrEmpty]
+        public List<string> AddressPrefix { get; set; }
+
+        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, HelpMessage = "The list of Dns Servers",
+            ParameterSetName = "clu")]
+        public List<string> DnsServer { get; set; }
+
+        public override void ExecuteCmdlet()
+        {
+            base.ExecuteCmdlet();
+
+            if (this.ParameterSetName.Equals("object"))
+            {
+                if (!this.IsVirtualNetworkPresent(this.VirtualNetwork.ResourceGroupName, this.VirtualNetwork.Name))
+                {
+                    throw new ArgumentException(Microsoft.Azure.Commands.Network.Properties.Resources.ResourceNotFound);
+                }
+
+                // Map to the sdk object
+                var vnetModel = Mapper.Map<MNM.VirtualNetwork>(this.VirtualNetwork);
+                vnetModel.Tags = TagsConversionHelper.CreateTagDictionary(this.VirtualNetwork.Tag, validate: true);
+
+                // Execute the Create VirtualNetwork call
+                this.VirtualNetworkClient.CreateOrUpdate(
+                    this.VirtualNetwork.ResourceGroupName,
+                    this.VirtualNetwork.Name,
+                    vnetModel);
+
+                var getVirtualNetwork = this.GetVirtualNetwork(
+                    this.VirtualNetwork.ResourceGroupName,
+                    this.VirtualNetwork.Name);
+                WriteObject(getVirtualNetwork);
+            }
+            else
+            {
+                WriteObject(this.CreateVirtualNetwork());
+            }
+        }
+
+        private PSVirtualNetwork CreateVirtualNetwork()
+        {
+            var vnet = this.GetVirtualNetwork(this.ResourceGroupName, this.Name);
+
+            vnet.Name = this.Name;
+            vnet.ResourceGroupName = this.ResourceGroupName;
+            vnet.AddressSpace = new PSAddressSpace();
+            vnet.AddressSpace.AddressPrefixes = this.AddressPrefix;
+
+            if (this.DnsServer != null)
+            {
+                vnet.DhcpOptions = new PSDhcpOptions();
+                vnet.DhcpOptions.DnsServers = this.DnsServer;
+            }
+
+            // Map to the sdk object
+            var vnetModel = Mapper.Map<MNM.VirtualNetwork>(vnet);
+            
+            // Execute the Create VirtualNetwork call
+            this.VirtualNetworkClient.CreateOrUpdate(this.ResourceGroupName, this.Name, vnetModel);
+
+            var getVirtualNetwork = this.GetVirtualNetwork(this.ResourceGroupName, this.Name);
+
+            return getVirtualNetwork;
+        }
+    }
+}
---
+++ src/CLU/Microsoft.Azure.Commands.Network/VirtualNetwork1/subnet1/AddAzureVirtualNetworkSubnetConfigCommand.cs
@@ -0,0 +1,142 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Management.Automation;
+using Microsoft.Azure.Commands.Network.Models;
+
+namespace Microsoft.Azure.Commands.Network
+{
+    using AutoMapper;
+
+    using Microsoft.Azure.Commands.Tags.Model;
+    using Microsoft.Azure.Management.Network;
+
+    [Cmdlet(VerbsCommon.Add, "AzureRmVirtualNetwork1Subnet1", DefaultParameterSetName = "Local"), OutputType(typeof(PSVirtualNetwork))]
+    [CliCommandAlias("vnet1 subnet1 add")]
+    public class AddAzureVirtualNetwork1Subnet1ConfigCommand : AzureVirtualNetwork1Subnet1ConfigBase
+    {
+        public override void ExecuteCmdlet()
+        {
+            base.ExecuteCmdlet();
+
+            if (this.ParameterSetName.Equals("Local"))
+            {
+                // Verify if the subnet exists in the VirtualNetwork
+                var subnet =
+                    this.VirtualNetwork.Subnets.SingleOrDefault(
+                        resource =>
+                        string.Equals(resource.Name, this.Name, System.StringComparison.CurrentCultureIgnoreCase));
+
+                if (subnet != null)
+                {
+                    throw new ArgumentException("Subnet with the specified name already exists");
+                }
+
+                if (string.Equals(ParameterSetName, Microsoft.Azure.Commands.Network.Properties.Resources.SetByResource))
+                {
+                    if (this.NetworkSecurityGroup != null)
+                    {
+                        this.NetworkSecurityGroupId = this.NetworkSecurityGroup.Id;
+                    }
+
+                    if (this.RouteTable != null)
+                    {
+                        this.RouteTableId = this.RouteTable.Id;
+                    }
+                }
+
+                subnet = new PSSubnet();
+
+                subnet.Name = this.Name;
+                subnet.AddressPrefix = this.AddressPrefix;
+
+                if (!string.IsNullOrEmpty(this.NetworkSecurityGroupId))
+                {
+                    subnet.NetworkSecurityGroup = new PSNetworkSecurityGroup();
+                    subnet.NetworkSecurityGroup.Id = this.NetworkSecurityGroupId;
+                }
+
+                if (!string.IsNullOrEmpty(this.RouteTableId))
+                {
+                    subnet.RouteTable = new PSRouteTable();
+                    subnet.RouteTable.Id = this.RouteTableId;
+                }
+
+                this.VirtualNetwork.Subnets.Add(subnet);
+
+                WriteObject(this.VirtualNetwork);
+            }
+            else
+            {
+                var vnet = this.GetVirtualNetwork(this.ResourceGroupName, this.VirtualNetworkName);
+                var subnet =
+                    vnet.Subnets.SingleOrDefault(
+                        resource =>
+                        string.Equals(resource.Name, this.Name, System.StringComparison.CurrentCultureIgnoreCase));
+
+                if (subnet != null)
+                {
+                    throw new ArgumentException("Subnet with the specified name already exists");
+                }
+
+                if (string.Equals(ParameterSetName, Microsoft.Azure.Commands.Network.Properties.Resources.SetByResource))
+                {
+                    if (this.NetworkSecurityGroup != null)
+                    {
+                        this.NetworkSecurityGroupId = this.NetworkSecurityGroup.Id;
+                    }
+
+                    if (this.RouteTable != null)
+                    {
+                        this.RouteTableId = this.RouteTable.Id;
+                    }
+                }
+
+                subnet = new PSSubnet();
+
+                subnet.Name = this.Name;
+                subnet.AddressPrefix = this.AddressPrefix;
+
+                if (!string.IsNullOrEmpty(this.NetworkSecurityGroupId))
+                {
+                    subnet.NetworkSecurityGroup = new PSNetworkSecurityGroup();
+                    subnet.NetworkSecurityGroup.Id = this.NetworkSecurityGroupId;
+                }
+
+                if (!string.IsNullOrEmpty(this.RouteTableId))
+                {
+                    subnet.RouteTable = new PSRouteTable();
+                    subnet.RouteTable.Id = this.RouteTableId;
+                }
+
+                vnet.Subnets.Add(subnet);
+
+                var vnetModel = Mapper.Map<Management.Network.Models.VirtualNetwork>(vnet);
+                vnetModel.Tags = TagsConversionHelper.CreateTagDictionary(vnet.Tag, validate: true);
+
+                // Execute the Create VirtualNetwork call
+                var getVirtualNetwork = this.VirtualNetworkClient.CreateOrUpdate(this.ResourceGroupName, vnet.Name, vnetModel);
+
+                var psVirtualNetwork = Mapper.Map<PSVirtualNetwork>(getVirtualNetwork);
+                psVirtualNetwork.ResourceGroupName = this.ResourceGroupName;
+
+                psVirtualNetwork.Tag = TagsConversionHelper.CreateTagHashtable(getVirtualNetwork.Tags);
+
+                WriteObject(psVirtualNetwork);
+            }
+        }
+    }
+}
